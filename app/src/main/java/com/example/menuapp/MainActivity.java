fccff02c3de40eec120bc577c998f855a0742efd
Nidhi <nidhidhanwariya@gmail.com>
2021-06-04 20:24:11 +0530

    New project

---
+++ app/src/main/java/com/example/menuapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.menuapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_menu);
+        registerForContextMenu(textView );
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+               startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                return true;
+            case R.id.item2:
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
